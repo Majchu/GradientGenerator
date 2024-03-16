18f91c9bfdb347e874158c338b9c59546893b5e3
Majchu <124588963+Majchu@users.noreply.github.com>
2024-03-16 20:01:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let color1 = document.getElementById("color1").value;
+let color2 = document.getElementById("color2").value;
+let range1 = document.getElementById("range1").value;
+let range2 = document.getElementById("range2").value;
+
